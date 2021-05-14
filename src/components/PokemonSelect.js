a9c9268bb22dfb1e142d98f8f7a9bed24308d454
hollyhbrady <hollyhbrady@gmail.com>
2021-05-14 18:47:01 +0100

    PokemonSelected started, pokemonOptions, handleChange pokemonSelected done

---
+++ src/components/PokemonSelect.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const PokemonSelect = ({pokemon, onPokemonSelected}) => {
+
+    const handleChange = function(event) {
+        const chosenPokemon = pokemon[event.target.value];
+        onPokemonSelected(chosenPokemon)
+    }
+
+    const pokemonOptions = pokemon.map((pokemon, index) => {
+        return <option key={index}>{pokemon.name}</option>
+    })
+
+    return (
+        <select defaultValue="" onChange={handleChange}>
+            <option value="" selected>Choose a Pokemon</option>
+            {pokemonOptions}
+        </select>
+    )
+};
+
+export default PokemonSelect;
--- src/containers/PokemonContainer.js
+++ src/containers/PokemonContainer.js
@@ -1,19 +1,28 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import PokemonSelect from '../components/PokemonSelect';
 
 const PokemonContainer = () => {
     const [pokemon, setPokemon] = useState([]);
+    const [selectedPokemon, setSelectedPokemon] = useState(null)
+
+    useEffect(() => {
+        getPokemon();
+    }, [])
 
     const getPokemon = function() {
         fetch('https://api.pokemontcg.io/v2/cards')
         .then(response => response.json())
-        .then(pokemon => setPokemon(pokemon))
+        .then(pokemon => setPokemon([pokemon]))
     }
 
+    const onPokemonSelected = function(pokemon){
+        setSelectedPokemon(pokemon);
+    }
 
     return (
         <>
         <h3>This is the Pokemon Container</h3>
-        {pokemon.name}
+        <PokemonSelect pokemon={pokemon} onPokemonSelected={onPokemonSelected}/>
         </>
     )
 }
